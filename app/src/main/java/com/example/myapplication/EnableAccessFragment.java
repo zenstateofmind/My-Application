fa04a66a19dd432315884337c80743cfb1d8075b
nikhiljoshi <nikhil.joshi92@gmail.com>
2020-04-25 16:14:39 -0700

    Added notification education screen

--- app/src/main/java/com/example/myapplication/EnableAccessFragment.java
+++ app/src/main/java/com/example/myapplication/EnableAccessFragment.java
@@ -14,6 +14,7 @@ import android.os.Bundle;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Handler;
 import android.provider.Settings;
@@ -40,8 +41,9 @@ import java.util.concurrent.TimeUnit;
 public class EnableAccessFragment extends Fragment {
 
     private static final String TAG = EnableAccessFragment.class.getSimpleName();
-    private int mInterval = 5   ;
+    private int mInterval = 5;
     private Handler mHandler;
+    private boolean goToNotifEducation = false;
 
     public EnableAccessFragment() {
         // Required empty public constructor
@@ -68,13 +70,21 @@ public class EnableAccessFragment extends Fragment {
         public void run() {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
                 if (!accessAllowed()) {
+                    goToNotifEducation = true;
                     mHandler.postDelayed(mStatusChecker, mInterval);
                     Log.i(TAG, "We will continue to go down the route of checking");
                 } else {
                     Log.i(TAG, "Yay! Seems like we got the access needed! Ideally we will jump to the next initiative " +
                             "from here");
-                    Intent intent = new Intent(getActivity(), HomeScreen.class);
-                    startActivity(intent);
+                    if (goToNotifEducation) {
+                        Intent intent = new Intent(getActivity(), NotificationEducation.class);
+                        startActivity(intent);
+                    } else {
+                        goToNotifEducation = false;
+                        Intent intent = new Intent(getActivity(), HomeScreen.class);
+                        startActivity(intent);
+
+                    }
                 }
             }
         }
@@ -108,7 +118,13 @@ public class EnableAccessFragment extends Fragment {
                 } else {
                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
 //                        printAppUsage();
-                        Intent intent = new Intent(getActivity(), HomeScreen.class);
+//                        Fragment enableNotificationsFragment = new EnableNotifications();
+//                        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+//                        transaction.replace(R.id.main_activity_fragment_container, enableNotificationsFragment ); // give your fragment container id in first parameter
+//                        transaction.addToBackStack(null);  // if written, this transaction will be added to backstack
+//                        transaction.commit();
+
+                        Intent intent = new Intent(getActivity(), NotificationEducation.class);
                         startActivity(intent);
 
                     }
@@ -116,135 +132,6 @@ public class EnableAccessFragment extends Fragment {
             }
         };
         enableUsageButton.setOnClickListener(buttonListener);
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-    private void printAppUsage() {
-        Log.i(TAG, "Usage access success!");
-
-        UsageStatsManager usageStatsManager = (UsageStatsManager) getActivity().getSystemService(Context.USAGE_STATS_SERVICE);
-        HashMap<String, Long> timeSpentPerApp = getTimeWithUsageEvents(usageStatsManager);
-
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-    private HashMap<String, Long> getTimeWithUsageEvents(UsageStatsManager usageStatsManager) {
-
-
-        PackageManager packageManager = getActivity().getPackageManager();
-        ArrayList<String> appsOnDevice = getInstalledApps(packageManager);
-        HashMap<String, ArrayList<UsageEvents.Event>> usageInfoEventsPerApp = new HashMap<String, ArrayList<UsageEvents.Event>>();
-        HashMap<String, Long> timeSpentPerApp = new HashMap<>();
-
-        // queryEvents takes start time and end time in milli seconds
-        UsageEvents usageEvents = usageStatsManager.queryEvents(1586415600000L, 1586493600000L);
-
-        while (usageEvents.hasNextEvent()) {
-            UsageEvents.Event currentEvent = new UsageEvents.Event();
-            usageEvents.getNextEvent(currentEvent);
-
-            if(currentEvent.getEventType() == UsageEvents.Event.MOVE_TO_FOREGROUND ||
-                    currentEvent.getEventType() == UsageEvents.Event.MOVE_TO_BACKGROUND) {
-
-                try {
-                    ApplicationInfo applicationInfo = packageManager.getApplicationInfo(currentEvent.getPackageName(), 0);
-                    String appName = applicationInfo.loadLabel(packageManager).toString();
-
-                    Log.i(TAG, "App Name: " + appName +
-                            " Event type: " + currentEvent.getEventType() +
-                            " Time: " + dateAndTime(currentEvent.getTimeStamp()));
-
-                    // If the usage event belongs to an installed app, add it to our map of <app -> events>
-                    if (appsOnDevice.contains(appName)) {
-                        ArrayList<UsageEvents.Event> appRelatedEvents = new ArrayList<>();
-                        if (usageInfoEventsPerApp.keySet().contains(appName)) {
-                            appRelatedEvents = usageInfoEventsPerApp.get(appName);
-                        }
-                        appRelatedEvents.add(currentEvent);
-                        usageInfoEventsPerApp.put(appName, appRelatedEvents);
-                    }
-
-                } catch (PackageManager.NameNotFoundException e) {
-                    e.printStackTrace();
-                }
-
-            }
-        }
-
-        timeSpentPerApp = getTimeSpentPerApp(usageInfoEventsPerApp);
-
-        for (String appName : timeSpentPerApp.keySet()) {
-            Log.i(TAG, "App name: " + appName + " Time spent: " + TimeUnit.MILLISECONDS.toMinutes(timeSpentPerApp.get(appName)));
-        }
-
-        return timeSpentPerApp;
-
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-    private HashMap<String, Long> getTimeSpentPerApp(HashMap<String, ArrayList<UsageEvents.Event>> usageInfoEventsPerApp) {
-
-        HashMap<String, Long> timeSpentPerApp = new HashMap<>();
-
-        for (String app : usageInfoEventsPerApp.keySet()) {
-
-            long timeSpent = 0;
-            int i = 0;
-            ArrayList<UsageEvents.Event> appEvents = usageInfoEventsPerApp.get(app);
-
-            // Get two events at once - ideally know thats a foreground task and one thats a
-            // background task. However, due to complications, this is not the case all the time,
-            // so we *check* if the first task is foreground task and the second task is the
-            // background task. If not, there might be some weird stuff happening with the way
-            // usage events have been saved. In that case, we increment by 1 and examine the same thing.
-            // If it's not the case, we take a time diff of background - foreground, add that
-            // to the time spent on the app and then increment by 2.
-
-            while (i < appEvents.size() - 1) {
-                UsageEvents.Event firstEvent = appEvents.get(i);
-                UsageEvents.Event secondEvent = appEvents.get(i + 1);
-                if (firstEvent.getEventType() == UsageEvents.Event.MOVE_TO_FOREGROUND &&
-                        secondEvent.getEventType() == UsageEvents.Event.MOVE_TO_BACKGROUND) {
-                    timeSpent += secondEvent.getTimeStamp() - firstEvent.getTimeStamp();
-                    Log.i(TAG, "App name: " + app +
-                            " first event: " + firstEvent.getEventType() +
-                            " first event time: " + dateAndTime(firstEvent.getTimeStamp()) +
-                            " second event: " + secondEvent.getEventType() +
-                            " second event time: " + dateAndTime(secondEvent.getTimeStamp()) +
-                            " Time Spent Calculation: " + TimeUnit.MILLISECONDS.toMinutes(timeSpent));
-                    i = i + 2;
-                } else {
-                    i = i + 1;
-                }
-            }
-
-            timeSpentPerApp.put(app, timeSpent);
-        }
-
-        return timeSpentPerApp;
-
-    }
-
-    private ArrayList<String> getInstalledApps(PackageManager packageManager) {
-        Intent intent = new Intent(Intent.ACTION_MAIN, null);
-        intent.addCategory(Intent.CATEGORY_LAUNCHER);
-        List<ResolveInfo> installedApplications = packageManager.queryIntentActivities(intent, PackageManager.GET_META_DATA);
-
-        ArrayList<String> appsOnDevice = new ArrayList<>();
-
-        // Convert the list of resolved info into app name string list
-        for (ResolveInfo appInfo: installedApplications) {
-            appsOnDevice.add(appInfo.loadLabel(packageManager).toString());
-        }
-        return appsOnDevice;
-    }
-
-    private String dateAndTime(Long timestamp) {
-        Date date = new Date(timestamp);
-        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy h:mm:ss a");
-        sdf.setTimeZone(TimeZone.getTimeZone("PST"));
-        String formattedDate = sdf.format(date);
-        return formattedDate;
     }
 
 
--- app/src/main/java/com/example/myapplication/EnableNotifications.java
+++ app/src/main/java/com/example/myapplication/EnableNotifications.java
@@ -2,8 +2,10 @@ package com.example.myapplication;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
@@ -35,32 +37,23 @@ public class EnableNotifications extends Fragment {
         // Inflate the layout for this fragment
         View enableNotificationsView = inflater.inflate(R.layout.fragment_enable_notifications, container, false);
 
-        enableComponents(enableNotificationsView);
+        initializeComponents(enableNotificationsView);
 
         return enableNotificationsView;
     }
 
-    private void enableComponents(View enableNotificationsView) {
-        Button enableNotifsButton = (Button)enableNotificationsView.findViewById(R.id.enable_notifs_button);
-        enableNotifsButton.setOnClickListener(new View.OnClickListener() {
+    private void initializeComponents(View enableNotificationsView) {
+
+        // Initialize the components
+        final Button enableUsageButton = enableNotificationsView.findViewById(R.id.enable_notifs_button);
+        View.OnClickListener buttonListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                Intent intent = new Intent();
-//                intent.setAction("android.settings.APP_NOTIFICATION_SETTINGS");
-//
-//                //for Android 5-7
-//                intent.putExtra("app_package", getActivity().getPackageName());
-//                intent.putExtra("app_uid", getActivity().getApplicationInfo().uid);
-//
-//                // for Android 8 and above
-//                intent.putExtra("android.provider.extra.APP_PACKAGE", getActivity().getPackageName());
-//
-//                startActivity(intent);
-
-                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:"+getActivity().getPackageName()));
-                startActivityForResult(intent, 0);
-
+                Intent intent = new Intent(getActivity(), HomeScreen.class);
+                startActivity(intent);
             }
-        });
+        };
+        enableUsageButton.setOnClickListener(buttonListener);
     }
+
 }
